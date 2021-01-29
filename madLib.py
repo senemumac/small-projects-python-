bc03cc598b706998034e7a84cba00fbd19198919
senemumac <78228726+senemumac@users.noreply.github.com>
2021-01-30 01:08:41 +0300

    Add files via upload

---
+++ madLib.py
@@ -0,0 +1,9 @@
+color = input("Enter a color: ")
+noun = input("Enter a plural noun: ")
+celebrity = input("Enter a celebrity: ")
+"""
+print("Roses are",color)
+print(noun+"are blue")
+print("I love",celebrity)
+"""
+print("Roses are {} are blue {}.I love {}".format(color ,noun,celebrity))
